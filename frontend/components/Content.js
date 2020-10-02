0c41f616349f945ba48fb3de17bf84f79be2c665
adamstroberg <stroberg@usc.edu>
2020-10-01 19:39:20 -0700

    update

--- frontend/components/Content.js
+++ frontend/components/Content.js
@@ -1,36 +1,37 @@
 // css
 import styles from './Content.module.css';
 
+// import
+import Profile from './Profile';
+
 export default function Content(props) {
   const showContent = () => {
-    // if(props.section === 'messages') {
-    //   return (
-    //     <h1>messages</h1>
-    //   );
-    // }
-    // else if(props.section === 'profile') {
-    //   return (
-    //     <h1>profile</h1>
-    //   );
-    // }
-    // else if(props.section === 'settings') {
-    //   return (
-    //     <h1>settings</h1>
-    //   );
-    // }
-    // else if(props.section === 'search') {
-    //   return (
-    //     <h1>search</h1>
-    //   );
-    // }
-    return (
-      <h1>{props.section}</h1>
-    )
+    if(props.section === 'messages') {
+      return (
+        <h1>messages</h1>
+      );
+    }
+    else if(props.section === 'profile') {
+      return (
+        <Profile  />
+      );
+    }
+    else if(props.section === 'settings') {
+      return (
+        <h1>settings</h1>
+      );
+    }
+    else if(props.section === 'search') {
+      return (
+        <h1>search</h1>
+      );
+    }
   }
 
   return (
     <div className={styles.content}>
-      {props.section}
+      {/* {props.section} */}
+      <Profile />
     </div>
   )
 }
