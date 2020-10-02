c09811bb4992eec165254bbbb810ca5ff0fd4fc5
jyoshio2 <galdones@usc.edu>
2020-10-01 19:09:51 -0700

    finished menu callback function

--- frontend/components/Menu.js
+++ frontend/components/Menu.js
@@ -7,10 +7,10 @@ import MenuItem from './MenuItem';
 export default function Menu(props) {
   return (
     <div className={styles.menu}>
-      <a onClick={props.updateSection('profile')}>Profile</a>
-      <a onClick={props.updateSection('messages')}>Messages</a>
-      <a onClick={props.updateSection('search')}>Search</a>
-      <a onClick={props.updateSection('settings')}>Settings</a>
+      <a onClick={() => props.updateSection('profile')}>Profile</a>
+      <a onClick={() => props.updateSection('messages')}>Messages</a>
+      <a onClick={() => props.updateSection('search')}>Search</a>
+      <a onClick={() => props.updateSection('settings')}>Settings</a>
     </div>
   );
 }
