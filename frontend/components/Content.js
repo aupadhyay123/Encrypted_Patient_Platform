ff84a38d3bb5ae4e16e024eb5abc5f078744fabc
jyoshio2 <galdones@usc.edu>
2020-10-01 19:40:11 -0700

    added dynamic rendering to content

--- frontend/components/Content.js
+++ frontend/components/Content.js
@@ -2,35 +2,20 @@
 import styles from './Content.module.css';
 
 export default function Content(props) {
-  const showContent = () => {
-    // if(props.section === 'messages') {
-    //   return (
-    //     <h1>messages</h1>
-    //   );
-    // }
-    // else if(props.section === 'profile') {
-    //   return (
-    //     <h1>profile</h1>
-    //   );
-    // }
-    // else if(props.section === 'settings') {
-    //   return (
-    //     <h1>settings</h1>
-    //   );
-    // }
-    // else if(props.section === 'search') {
-    //   return (
-    //     <h1>search</h1>
-    //   );
-    // }
-    return (
-      <h1>{props.section}</h1>
-    )
-  }
-
   return (
     <div className={styles.content}>
-      {props.section}
+      {props.section === 'messages' &&
+        <h1>messages</h1>
+      }
+      {props.section === 'profile' &&
+        <Profile />
+      }
+      {props.section === 'settings' &&
+        <h1>settings</h1>
+      }
+      {props.section === 'search' &&
+        <h1>search</h1>
+      }
     </div>
   )
 }
