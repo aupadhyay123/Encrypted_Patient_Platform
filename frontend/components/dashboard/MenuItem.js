5d62bd05f84404fa6411a32473eb1a8917eedad1
Atharva Upadhyay <aaupadhy@usc.edu>
2020-11-04 15:04:35 -0800

    aesthetic stuff for frontend

--- frontend/components/dashboard/MenuItem.js
+++ frontend/components/dashboard/MenuItem.js
@@ -1,6 +1,7 @@
 // redux
 import { connect } from 'react-redux';
 import { changeSection } from '../../actions/section';
+import styles from './MenuItem.module.css';
 
 function MenuItem(props) {
   const IconStyle = {
@@ -8,14 +9,13 @@ function MenuItem(props) {
   }
 
   return (
-    <a onClick={() => props.selectSection(props.name)}>
+    <a className={styles.menuItemContainer} onClick={() => props.selectSection(props.name)}>
       <img src={props.icon} style={IconStyle} />
+      <span className={styles.tooltip}>{props.name}</span>
     </a>
   );
 }
-
 const mapDispatchToProps = (dispatch) => ({
   selectSection: (section) => dispatch(changeSection(section)),
 });
-
 export default connect(null, mapDispatchToProps)(MenuItem);
